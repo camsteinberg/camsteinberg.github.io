5d95e7ee8ff1cfb519ce22cfbc4954a26940b6fd
Cam Steinberg <striker688@gmail.com>
2022-12-05 02:42:49 -0500

    Added contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,11 @@ import React from "react";
 function Contact() {
     return (
         <div className="flex flex-col mb-10 mx-auto">
+            <h2 className="flex justify-center mb-2 text-2xl ">Contact Form</h2>
             <div className="flex justify-center
             items-center">
                 <form
-                    action="https://getform.io/slug"
+                    action="https://getform.io/f/75f73df5-79f2-4bbe-928c-dfd0b65b24fd"
                     method="POST"
                     className="flex flex-col w-full md:w-7/12"
                 >
@@ -17,6 +18,22 @@ function Contact() {
                         className="my-2 p-2 bg-transparent
                         border-2 rounded-md focus:outline-none"
                     />
+                    <textarea
+                        name="message"
+                        placeholder="Message"
+                        rows="10"
+                        className="p-2 mb-4 bg-transparent border-2 rounded-md
+                        focus:outline-none"
+                    ></textarea>
+                    <button
+                        type="button"
+                        className="text-center inline-block
+                        px-8 py-3 w-max text-base font-medium
+                        rounded-md text-white bg-gradient-to-r
+                        from-yellow-500 to-pink-500 drop-shadow-md
+                        hover:stroke-white">
+                        Work With Me
+                    </button>
                 </form>
             </div>
         </div>
